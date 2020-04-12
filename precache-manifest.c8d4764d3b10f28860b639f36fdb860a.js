963e4f25b72fab6878e6cd74d09dd0bc9b5d320c
Enzo de Carvalho Santos <enzocsantos18@gmail.com>
2020-04-12 20:17:07 -0300

    Updates

---
+++ precache-manifest.c8d4764d3b10f28860b639f36fdb860a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14a91b052bf2e44a142e18fde4ec6371",
+    "url": "/todo-context/index.html"
+  },
+  {
+    "revision": "1a4ebc181af51e9de26d",
+    "url": "/todo-context/static/css/main.d5704542.chunk.css"
+  },
+  {
+    "revision": "539b8849014aac044b5c",
+    "url": "/todo-context/static/js/2.d2420658.chunk.js"
+  },
+  {
+    "revision": "90526b3367ca000531e0465e56eee9ab",
+    "url": "/todo-context/static/js/2.d2420658.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a4ebc181af51e9de26d",
+    "url": "/todo-context/static/js/main.d6226691.chunk.js"
+  },
+  {
+    "revision": "ef6e7728369a957f9f8d",
+    "url": "/todo-context/static/js/runtime-main.a6f5d608.js"
+  }
+]);
